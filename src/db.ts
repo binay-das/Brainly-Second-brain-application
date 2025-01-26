75052754de5f3208436f301a5147f05604b35547
Binay <binaydas1123@gmail.com>
2025-01-26 09:12:32 +0530

    Link Schema

--- src/db.ts
+++ src/db.ts
@@ -42,5 +42,19 @@ const ContentSchema = new mongoose.Schema({
     }
 });
 
+const LinkSchema = new mongoose.Schema({
+    hash: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true
+    }
+});
+
 export const User = mongoose.model("User", UserSchema);
 export const Content = mongoose.model("Content", ContentSchema);
+export const Link = mongoose.model("Link", LinkSchema);
