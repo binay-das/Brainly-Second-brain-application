f6a07ad4ee9cbad3836d0252e0ccba79303638f0
Binay <binaydas1123@gmail.com>
2025-01-28 01:12:06 +0530

    Sidebar

--- Frontend/src/components/Sidebar.tsx
+++ Frontend/src/components/Sidebar.tsx
@@ -1,10 +1,16 @@
 import { SidebarItem } from "./SidebarItem"
+import { DocumentIcon } from "./ui/icons/DocumentIcon"
+import { LinkIcon } from "./ui/icons/LinkIcon"
+import { TagsIcon } from "./ui/icons/TagsIcon"
 import { XIcon } from "./ui/icons/XIcon"
 import { YoutubeIcon } from "./ui/icons/YoutubeIcon"
 
 export const Sidebar = () => {
     return <div className="w-60 h-screen bg-gray-200 fixed left-0 top-0">
         <SidebarItem icon={<XIcon />} title="X" />
-        <SidebarItem icon={<YoutubeIcon />} title="YouTube" />
+        <SidebarItem icon={<YoutubeIcon />} title="Videos" />
+        <SidebarItem icon={<DocumentIcon />} title="Documents" />
+        <SidebarItem icon={<LinkIcon />} title="Links" />
+        <SidebarItem icon={<TagsIcon />} title="Tags" />
     </div>
 }
