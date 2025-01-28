e59c9f7c6b0395dedeb1d66d85df381053302830
Binay <binaydas1123@gmail.com>
2025-01-28 10:57:00 +0530

    New content modal redesigned

--- Frontend/src/components/NewContentModal.tsx
+++ Frontend/src/components/NewContentModal.tsx
@@ -3,22 +3,40 @@ import { CloseIcon } from "./ui/icons/CloseIcon";
 import { InputComponent } from "./ui/InputComponent";
 
 interface NewContentModalProps {
-    open: boolean;
-    onClose: () => void;
+  open: boolean;
+  onClose: () => void;
 }
 
-export const NewContentModal = ({open, onClose}: NewContentModalProps) => {
-    return <div>
-        {open && <div className="w-screen h-screen bg-gray-700 opacity-75 fixed flex justify-center items-center">
-            <div className="bg-white opacity-100 flex flex-col justify-center items-center py-4 px-8 rounded-md">
-                <div onClick={onClose}><CloseIcon className={"flex justify-end w-full"} /></div>
-                <div>
-                    <InputComponent placeholder={'Title'} />
-                    <InputComponent placeholder={'Link'} />
-                </div>   
-                <Button variant='primary' size='md' text='Add Content' onClick={()=>console.log('Hi')} />
-            </div>    
-        </div>}
+export const NewContentModal = ({ open, onClose }: NewContentModalProps) => {
+  return (
+    <div>
+      {open && (
+        <div className="w-screen h-screen bg-gray-700 opacity-85 fixed flex justify-center items-center z-99">
+          <div className="bg-white opacity-100 flex flex-col justify-center items-center py-4 px-8 rounded-md">
+            <div onClick={onClose} className={"flex justify-end w-full mb-4"}>
+              <CloseIcon className={""} />
+            </div>
+            <div className="flex flex-col gap-4">
+              <InputComponent placeholder={"Title"} />
+              <InputComponent placeholder={"Link"} />
+              <Button
+                variant="primary"
+                size="md"
+                text="Add Content"
+                onClick={() => console.log("Hi")}
+              />
+              <div>
+                <input type="radio" name="youtube" id="youtube" />
+                <label htmlFor="youtube">Youtube Video</label>
+                <input type="radio" name="twitter" id="twitter" />
+                <label htmlFor="twitter">Tweet</label>
+                <input type="radio" name="document" id="document" />
+                <label htmlFor="document">Document</label>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-
-}
+  );
+};
