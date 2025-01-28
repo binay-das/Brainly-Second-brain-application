5e2a301bae01c74a3e4314fe5787767369d80cd0
Binay <binaydas1123@gmail.com>
2025-01-28 10:56:16 +0530

    Card data fetch

--- Frontend/src/pages/MainPage.tsx
+++ Frontend/src/pages/MainPage.tsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from "react";
+import { NewContentModal } from "../components/NewContentModal";
 import { Sidebar } from "../components/Sidebar";
 import { Button } from "../components/ui/Button";
 import { Card } from "../components/ui/Card";
 import { PlusIcon } from "../components/ui/icons/PlusIcon";
 import { ShareIcon } from "../components/ui/icons/ShareIcon";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 export const MainPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  interface CardData {
+    title: string;
+    link: string;
+    type: "twitter" | "youtube" | "document" | "link";
+  }
+
+  const [cardData, setCardData] = useState<CardData[]>([]);
+
+  const getCardData = async () => {
+    // console.log(localStorage.getItem('token'));
+    const response = await axios.get(`${BACKEND_URL}/api/v1/content`, {
+      headers: {
+        Authorization: `${localStorage.getItem('token')}`
+      }
+    });
+    setCardData(response.data);
+  }
+
+  useEffect(() => {
+    getCardData();
+  }, []);
+
   return (
     <div className="flex">
+      <NewContentModal open={isModalOpen} onClose={() => setIsModalOpen(false)}  />
       <Sidebar />
       <div className="flex flex-col ml-60 w-full p-4">
         <div className="flex justify-between items-center">
@@ -23,6 +51,7 @@ export const MainPage = () => {
               size="md"
               text="Add Content"
               startIcon={<PlusIcon size="md" />}
+              onClick={() => setIsModalOpen(true)}
             />
           </div>
         </div>
@@ -30,6 +59,10 @@ export const MainPage = () => {
         <div className="w-full flex gap-4 mt-4">
             <Card title="Project Ideas" link="yt.com" type="youtube" />
             <Card title="How to build a Seond Brain" link="yt.com" type="document"  />
+
+            {cardData.map((eachCardData) => <div>
+              <Card title={eachCardData.title} link={eachCardData.link} type={eachCardData.type}/>
+            </div>)}
         </div>
       </div>
     </div>
