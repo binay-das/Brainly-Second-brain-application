ae90520129220426883c58dff6063b2b562b5a3e
Binay <binaydas1123@gmail.com>
2025-02-16 13:19:10 +0530

    Sidebar

--- Frontend/src/components/Sidebar.tsx
+++ Frontend/src/components/Sidebar.tsx
@@ -1,16 +1,69 @@
-import { SidebarItem } from "./SidebarItem"
-import { DocumentIcon } from "./ui/icons/DocumentIcon"
-import { LinkIcon } from "./ui/icons/LinkIcon"
-import { TagsIcon } from "./ui/icons/TagsIcon"
-import { XIcon } from "./ui/icons/XIcon"
-import { YoutubeIcon } from "./ui/icons/YoutubeIcon"
+import { useEffect, useState } from "react";
+import { SidebarItem } from "./SidebarItem";
+import { DocumentIcon } from "./ui/icons/DocumentIcon";
+import { LinkIcon } from "./ui/icons/LinkIcon";
+import { TagsIcon } from "./ui/icons/TagsIcon";
+import { XIcon } from "./ui/icons/XIcon";
+import { YoutubeIcon } from "./ui/icons/YoutubeIcon";
+import { HamburgerIcon } from "./ui/icons/HamburgerIcon";
+
+const useMediaQuery = (query: string) => {
+  const [matches, setMatches] = useState<boolean>(
+    window.matchMedia(query).matches
+  );
+
+  useEffect(() => {
+    const isMatch = window.matchMedia(query);
+    if (isMatch.matches !== matches) {
+      setMatches(isMatch.matches);
+    }
+
+    const listener = () => setMatches(isMatch.matches);
+    isMatch.addEventListener("change", listener);
+
+    return () => {
+      isMatch.removeEventListener("change", listener);
+    };
+  }, [query]);
+
+  return matches;
+};
 
 export const Sidebar = () => {
-    return <div className="w-60 h-screen bg-gray-200 fixed left-0 top-0">
-        <SidebarItem icon={<XIcon />} title="X" />
+  const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  useEffect(() => {
+    setSidebarOpen(isDesktop);
+  }, [isDesktop]);
+
+  return (
+    <>
+      <div onClick={() => setSidebarOpen(!sidebarOpen)}>
+        <HamburgerIcon />
+      </div>
+      <div
+        className={`bg-gray-200 min-h-screen transition-transform duration-300 ease-in-out ${
+          sidebarOpen ? "w-60" : "w-0 md:w-0"
+        } 
+    ${isDesktop ? "relative" : "fixed top-0 left-0 h-full z-40 shadow-lg"}
+    ${sidebarOpen ? "translate-x-0" : "-translate-x-full"} overflow-hidden`}
+      >
+        {!isDesktop && sidebarOpen && (
+          <div
+            className="p-2 cursor-pointer"
+            onClick={() => setSidebarOpen(false)}
+          >
+            <HamburgerIcon />
+          </div>
+        )}
+
+        <SidebarItem icon={<XIcon />} title="Tweet" />
         <SidebarItem icon={<YoutubeIcon />} title="Videos" />
         <SidebarItem icon={<DocumentIcon />} title="Documents" />
         <SidebarItem icon={<LinkIcon />} title="Links" />
         <SidebarItem icon={<TagsIcon />} title="Tags" />
-    </div>
-}
+      </div>
+    </>
+  );
+};
