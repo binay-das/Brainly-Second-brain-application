945e5993fc718900bd1c682e4daa127376b25a64
Binay <binaydas1123@gmail.com>
2025-02-23 11:33:29 +0530

    Dimensions changes

--- Frontend/src/components/ui/Card.tsx
+++ Frontend/src/components/ui/Card.tsx
@@ -19,7 +19,7 @@ interface CardProps {
 export const Card = (props: CardProps) => {
   const navigate = useNavigate();
   return (
-    <div className="h-40 w-40  bg-gray-600 text-gray-300 rounded-3xl shadow-md cursor-pointer">
+    <div className="h-44 w-44  text-gray-300 rounded-3xl shadow-md cursor-pointer">
       <div className="h-1/3 w-full px-2 bg-[#2C2C2C]  rounded-t-3xl flex justify-between">
         <div className="flex items-center gap-2 ">
           {props.type == "twitter" ? (
@@ -40,17 +40,16 @@ export const Card = (props: CardProps) => {
         </div>
       </div>
 
-      <div className="h-2/3 w-full px-2 bg-[#252525] rounded-b-3xl">
+      <div className="h-2/3 w-full px-2 bg-[#252525] rounded-b-3xl   relative">
         <div
-          className="w-full h-full p-2"
+          className="w-full h-full px-2  "
           onClick={() => navigate(`/content/${props.id}`)}
         >
-          <div className="text-md font-medium">{props.title}</div>
-          <div className="h-1/2 w-full text-xs mt-1 opacity-50 line-clamp-2">
+          <div className="text-md font-medium  ">{props.title}</div>
+          <div className=" w-full text-xs mt-1 opacity-50 line-clamp-2  ">
             {props.description}
           </div>
-          
-          <div>{props.tags}</div>
+          <div className="absolute bottom-0 left-0 w-full text-xs text-gray-400 p-1">{props.tags?.join(", ")}</div>
         </div>
       </div>
     </div>
